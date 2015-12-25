1dfdcb9c19b45f4d3e68fec1990444e495080484
Patricio López <lopezjuripatricio@gmail.com>
2015-12-25 20:29:42 -0300

    add  base, models and settings

---
+++ app/app.js
@@ -0,0 +1,30 @@
+// Node imports
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+// Local imports
+const config = require('./config');
+const database = require('./config/database');
+const models = require('./models');
+
+// Webapp
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.enable('trust proxy');
+
+// Logs
+app.use(morgan('dev'));
+
+// Index
+app.get("/", (req, res) => {
+    res.send({ status: 'on' });
+});
+
+// REST API Routes
+app.use('/api', require('./routes/api'));
+
+module.exports.server = app;
+module.exports.config = config;
+module.exports.database = database;
---
+++ app/config/index.js
@@ -0,0 +1,21 @@
+const nconf = require('nconf');
+
+const settings = {
+  'PRODUCTION': 'app/config/env/production.json',
+  'DEVELOPMENT': 'app/config/env/development.json',
+  'TEST': 'app/config/env/test.json',
+}
+
+const env = process.env.NODE_ENV || 'DEVELOPMENT';
+const file = settings[env.toUpperCase()];
+
+if (!file) {
+  console.log('Environment settings not found');
+}
+
+module.exports = nconf.env().argv().file(file).defaults({
+  'port': 3000,
+  'mongo': {
+    'uri': 'mongodb://localhost:27017/uccourses',
+  },
+});
