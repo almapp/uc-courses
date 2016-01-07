6c8f6edbbf6a20af869557a3ef18decfdda11a5e
Patricio López <lopezjuripatricio@gmail.com>
2016-01-07 14:28:05 -0300

    remove deploy.sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-if [ ! -z "$TRAVIS_TAG" ]; then
-  docker tag $LOCAL_IMAGE:latest $REGISTRY_IMAGE:$TRAVIS_TAG
-  docker push $LOCAL_IMAGE
-elif [ "$TRAVIS_BRANCH" == "master" ]; then
-  docker tag $LOCAL_IMAGE:latest $REGISTRY_IMAGE:latest
-  docker push $LOCAL_IMAGE
-fi
