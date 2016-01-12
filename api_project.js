60c6f5be7c4074c24db1706f3cfcd71778a0860f
Patricio López <lopezjuripatricio@gmail.com>
2016-01-12 12:42:43 -0300

    add docs

---
+++ api_project.js
@@ -0,0 +1,1 @@
+define({  "title": "uc-courses",  "url": "/api/v1",  "sampleUrl": "http://uc-courses.lopezjuri.com/api/v1",  "name": "uc-courses",  "version": "2.2.0",  "description": "UC Courses API",  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-01-12T15:39:55.791Z",    "url": "http://apidocjs.com",    "version": "0.14.0"  }});
