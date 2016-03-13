6f7f0cf28e40c6926a2a42b0b085c51a04906d30
Patricio López <lopezjuripatricio@gmail.com>
2016-03-12 22:20:11 -0300

    simplify scrap task and make it a private operation

--- app/tasks/scrap/index.js
+++ app/tasks/scrap/index.js
@@ -2,25 +2,35 @@
 
 const mongoose = require('mongoose');
 const Promise = require('bluebird');
-const scraper = require('buscacursos-uc-scraper');
+const buscacursos = require('buscacursos-uc');
 
 const database = require('./../../config/database');
 const models = require('../../models');
 
 const Course = mongoose.model('Course');
 
-
-module.exports = function(options) {
-  const initials = options.initials || scraper.initials;
-  return Course.remove({})
-    .then(() => {
-      function handler(initial) {
-        return scraper.deepSearch([initial], options).then(courses => {
-          return Course.create(courses);
-        });
-      }
-      return Promise.map(initials, handler, { concurrency: 1 });
-    }).then(created => {
-      return created.length;
-    });
+// Handler per query
+function fetch(query) {
+  return buscacursos.fetch(query)
+    .then(courses => Course.create(courses));
 }
+
+function scrap(options) {
+  options = options || {}
+  const initials = options.initials || require('./initials.json');
+  const year = options.year || 2016;
+  const period = options.period || 1;
+
+  const queries = initials.map(initial => ({
+    'cxml_semestre': `${year}-${period}`,
+    'cxml_sigla': initial,
+  }));
+
+  return Course.remove({})
+    .then(() => Promise.map(queries, fetch, { concurrency: 5 }));
+}
+
+// Start operation
+scrap().then(result => {
+  return console.log('Scraping done! Batches:', result.length);
+}).then(() => process.exit())
--- index.js
+++ index.js
@@ -1,37 +1,8 @@
 'use strict';
 
-const schedule = require('node-schedule');
-
-const scrap = require('./app/tasks/scrap');
-
 const app = require('./app/app');
 const server = app.server;
 const config = app.config;
-
-function startScraping(options) {
-  console.log('Scraping started');
-  scrap(options).then(ammount => {
-    console.log('Scraped:', ammount);
-  }).catch(err => {
-    console.error(err);
-  })
-}
-
-const job = schedule.scheduleJob('0 0 2 ? * SAT *', () => {
-  startScraping({ year: 2016, period: 1 });
-});
-
-if (config.get('scrap:secret')) {
-  server.get(`/${config.get('scrap:secret')}`, (req, res, next) => {
-    const options = {
-      year: req.query.year || 2016,
-      period: req.query.period || 1,
-      initials: req.query.initials, // null if not present
-    };
-    startScraping(options);
-    res.send(`Scraping started on ${options.year}-${options.period}`);
-  });
-}
 
 // Start server
 server.listen(config.get('port'), () => {
